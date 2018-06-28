c265f840037299eaa810c3bdaab943663eb10190
chhull <ch.hull@verizon.net>
2018-06-28 08:44:48 -0400

    still trying

--- queries.js
+++ queries.js
@@ -4,3 +4,23 @@ const db = pg(conString);
 const url = require('url');
 
 var queries = {};
+queries.getPulse = function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var id = parseInt(query.id);
+    console.log(id);
+    // query database
+    db.any('SELECT pusle FROM person WHERE id = $1', [id]) // returns promise
+      .then((results)=> {
+        console.log(results)
+        res.status(200)
+           .json(results)
+      })
+      .catch((err)=> {
+          console.log(err)
+          res.status(400)
+             .json({"error":"Person does not exist."})
+      })
+    // display data on server for that id
+    
+}
