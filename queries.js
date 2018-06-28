4cb8b2cea0a1aaecff250f094c1bc3759e6e80e3
chhull <ch.hull@verizon.net>
2018-06-28 09:22:54 -0400

    added queries

--- queries.js
+++ queries.js
@@ -1,11 +1,11 @@
-/*const pg = require('pg-promise')({});
-//var conString = 'process.env.DATABASE_URL'; // replace with heroku
-//const db = pg(conString);
-//const url = require('url');
+const pg = require('pg-promise')({});
+var conString = 'process.env.DATABASE_URL'; // replace with heroku
+const db = pg(conString);
+const url = require('url');
 
 var queries = {};
 queries.getPulse = function (req, res) {
-    /*var url_parts = url.parse(req.url, true);
+    var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     var id = parseInt(query.id);
     console.log(id);
@@ -22,5 +22,5 @@ queries.getPulse = function (req, res) {
              .json({"error":"Person does not exist."})
       })
     // display data on server for that id
-    */
-/*}*/
+    
+}
