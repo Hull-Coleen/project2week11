3f7aa22b1f2eba65035621e5a3f289d934919763
chhull <ch.hull@verizon.net>
2018-06-28 11:15:10 -0400

    help:

--- queries.js
+++ queries.js
@@ -2,8 +2,28 @@ const pg = require('pg-promise')({});
 var conString = process.env.DATABASE_URL; // replace with heroku
 const db = pg(conString);
 const url = require('url');
-var queries = {};
+/*var queries = {};
 queries.getPulse = function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var id = parseInt(query.input);
+    console.log(id);
+    // query database
+    db.any('SELECT pusle FROM person WHERE id = $1', [id]) // returns promise
+      .then((results)=> {
+        console.log(results)
+        res.status(200)
+           .json(results)
+      })
+      .catch((err)=> {
+          console.log(err)
+          res.status(400)
+             .json({"error":"Person does not exist."})
+      })
+    // display data on server for that id
+    
+}*/
+function getPulse (req, res) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     var id = parseInt(query.input);
