17d48115da4c6207b2a117dab8cd98cec088586b
chhull <ch.hull@verizon.net>
2018-06-28 10:41:14 -0400

    j

--- index.js
+++ index.js
@@ -11,10 +11,53 @@ app.set('view engine', 'ejs');
 app.get('/', (req,res)=> {
     res.render('pages/index', {title: "home"})
 });
-app.get('/getPulse', queries.getPulse);
+app.get('/getPulse', getPulse);
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 function getPusle(req, res) {
+	var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var id = parseInt(query.input);
+    console.log(id);
+    // query database
+    db.any('SELECT pusle FROM person WHERE id = $1', [id]) // returns promise
+      .then((results)=> {
+        console.log(results)
+        res.status(200)
+           .json(results)
+      })
+      .catch((err)=> {
+          console.log(err)
+          res.status(400)
+             .json({"error":"Person does not exist."})
+      })
+    // display data on server for that id
 }
+
+const pg = require('pg-promise')({});
+var conString = process.env.DATABASE_URL; // replace with heroku
+const db = pg(conString);
+const url = require('url');
+var queries = {};
+/*queries.getPulse = function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var id = parseInt(query.input);
+    console.log(id);
+    // query database
+    db.any('SELECT pusle FROM person WHERE id = $1', [id]) // returns promise
+      .then((results)=> {
+        console.log(results)
+        res.status(200)
+           .json(results)
+      })
+      .catch((err)=> {
+          console.log(err)
+          res.status(400)
+             .json({"error":"Person does not exist."})
+      })
+    // display data on server for that id
+    
+}*/
 /*express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
