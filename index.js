c259dc793fc5151415299e48d6c116e62a98e4d4
chhull <ch.hull@verizon.net>
2018-06-29 20:46:25 -0400

    tyring to use post

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/getPulse', getPulse);
 app.get('/getExercise', getExercise);
 app.get('/getWeight', getWeight);
 app.get('/getUser', getUser);
-app.post('/signin', getUserName);
+app.post('/', getUserName);
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 function getPulse(req, res) {
